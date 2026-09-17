25d266b0a83ef17fb2b643fbdb2d130156c0df8b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-17 04:17:41 +0200

    oh thats not a function

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function updateCountdown() {
   // const m = Math.floor((diff / (1000 * 60)) % 60);
   // const s = Math.floor((diff / 1000) % 60);
 
-  document.querySelector("time").dateTime = diff;
+  document.querySelector("time").dateTime = diff.toISOString();
 }
 
 setInterval(updateCountdown, 1000);

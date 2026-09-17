721e4de2efaf81441ab76ce6eb43c07d243d464d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-17 04:39:18 +0200

    use tabs intead

--- index.js
+++ index.js
@@ -2,22 +2,14 @@ const bdayMonth = 9;
 const bdayDay = 16;
 
 function getNextBirthday() {
-  const now = new Date();
-  let next = new Date(now.getFullYear(), bdayMonth - 1, bdayDay);
-  if (next < now) next.setFullYear(next.getFullYear() + 1);
-  return next;
+	const now = new Date();
+	let next = new Date(now.getFullYear(), bdayMonth - 1, bdayDay);
+	if (next < now) next.setFullYear(next.getFullYear() + 1);
+	return next;
 }
 
 function updateCountdown() {
-  const now = new Date();
-  const diff = getNextBirthday() - now;
-
-  // const d = Math.floor(diff / (1000 * 60 * 60 * 24));
-  // const h = Math.floor((diff / (1000 * 60 * 60)) % 24);
-  // const m = Math.floor((diff / (1000 * 60)) % 60);
-  // const s = Math.floor((diff / 1000) % 60);
-
-  document.querySelector("time").dateTime = new Date(diff).toISOString();
+	el.dateTime = getNextBirthday().toISOString();
 }
 
 setInterval(updateCountdown, 1000);
